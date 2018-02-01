08afedf12c193e03c92ec55594207d32fdb39351
samerty7 <samueljbaker@lewisu.edu>
2018-02-01 08:58:30 -0600

    Add files via upload

---
+++ Homework 2.py
@@ -0,0 +1,11 @@
+#Homework 2
+#Samuel Baker
+
+Things=["Apples","Pencils","Homework"]
+for loopStuff in Things:
+    print(loopStuff)
+print("")
+myMap={0:"This",1:"homework",2:"is",3:"DONE"}
+for loopMap in myMap:
+    print(myMap[loopMap])
+    
